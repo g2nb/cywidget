15772da5cdcb952c3ac6312357352c071725c2bc
tmtabor <tmtabor@cloud.ucsd.edu>
2023-04-04 09:11:49 -0700

    Adding support for remote URLs

--- cywidget/__init__.py
+++ cywidget/__init__.py
@@ -1,6 +1,9 @@
 from nbtools import build_ui
 from IPython.display import display
+from urllib.request import urlopen
+import builtins
 import json
+import re
 
 
 __version__ = '1.0.0'
@@ -19,13 +22,27 @@ __version__ = '1.0.0'
               }
           })
 def display_cytoscape_network(cx_file=''):
+    # Determine if the input is a file path or URL
+    is_url = re.compile(
+        r'^(?:http|ftp)s?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+    # Set the opener based on local or remote
+    if re.match(is_url, cx_file): open_file = urlopen
+    else: open_file = builtins.open
+
+    # Read the contents of the file
     data = ''
-    with open(cx_file) as f:
+    with open_file(cx_file) as f:
         data = f.read()
 
+    # Display the network
     data_json = json.loads(data)
     cx_data = {
         "application/cx": data_json
     }
-
     display(cx_data, raw=True)
